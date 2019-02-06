ba5cf7fade53c3b234d7f0af01de246e21e6ae26
Andre Rosot <alrosot@gmail.com>
2019-02-07 09:18:15 +1100

    Intercepting signals SIGHUP, SIGINT and SIGTERM

--- app.js
+++ app.js
@@ -17,7 +17,30 @@ app.use('/file-saver', express.static('node_modules/file-saver'));
 
 app.use('/popper', express.static('node_modules/popper.js/dist'));
 
-app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
         console.log('Selektion up and running!')
     }
 );
+
+// The signals we want to handle
+// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+var signals = {
+    'SIGHUP': 1,
+    'SIGINT': 2,
+    'SIGTERM': 15
+};
+// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+    console.log("shutdown!");
+    server.close(() => {
+        console.log(`server stopped by ${signal} with value ${value}`);
+        process.exit(128 + value);
+    });
+};
+// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+        console.log(`process received a ${signal} signal`);
+        shutdown(signal, signals[signal]);
+    });
+});
