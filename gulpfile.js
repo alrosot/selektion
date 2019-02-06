8ba0d8b61b3b46717f945a3d2fa075d3c64b870c
Andre Rosot <alrosot@gmail.com>
2019-02-07 09:40:45 +1100

    Fixing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 var gulp = require('gulp')
 var nodemon = require('gulp-nodemon');
 
-gulp.task('dev', [],
-    function () {
-        nodemon({
-            script: 'app.js',
-            ext: 'js',
-            ignore: ['client/*']
-        })
-            .on('restart',
-                function () {
-                    console.log('>> node restart ');
-                }
-            )
-    }
-);
+function dev() {
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        ignore: ['client/*']
+    })
+        .on('restart',
+            function () {
+                console.log('>> node restart ');
+            }
+        )
+}
 
-gulp.task('default', ['dev']);
+
+exports.default = dev
+
